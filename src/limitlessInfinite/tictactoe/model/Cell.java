d07732d5d4315ac1d104e70501287fefbf98da69
Limitless <1218bogdan@gmail.com>
2022-06-23 18:30:15 +0300

    Add model classes

--- src/limitlessInfinite/tictactoe/model/Cell.java
+++ src/limitlessInfinite/tictactoe/model/Cell.java
@@ -16,23 +16,23 @@
 
 package limitlessInfinite.tictactoe.model;
 
-public class GameTable {
+public class Cell {
 
-    private final char[][] table = {
-            {' ', ' ', ' '},
-            {' ', ' ', ' '},
-            {' ', ' ', ' '}
-    };
-
-    public boolean isEmpty(final Cell cell) {
-        return table[cell.getRow()][cell.getCol()] == ' ';
+    public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 
-    public char getSign(final Cell cell) {
-        return table[cell.getRow()][cell.getCol()];
+    public int getRow() {
+        return row;
     }
 
-    public void setSign(final Cell cell, final char sign) {
-        table[cell.getRow()][cell.getCol()] = sign;
+    public int getCol() {
+        return col;
     }
+
+    private final int row;
+
+    private final int col;
+
 }
