2d1e5ba3c38407ba5168202a08bc342dd33e9798
Limitless <1218bogdan@gmail.com>
2022-07-07 17:05:56 +0300

    Update  TerminalNumericKeypadCellNumberConverter

--- src/limitlessInfinite/tictactoe/component/keypad/TerminalNumericKeypadCellNumberConverter.java
+++ src/limitlessInfinite/tictactoe/component/keypad/TerminalNumericKeypadCellNumberConverter.java
@@ -20,26 +20,14 @@ import limitlessInfinite.tictactoe.component.CellNumberConverter;
 import limitlessInfinite.tictactoe.model.Cell;
 
 public class TerminalNumericKeypadCellNumberConverter implements CellNumberConverter {
-    private final char[][] table = {
-            {'1', '2', '3'},
-            {'4', '5', '6'},
-            {'7', '8', '9'}
-    };
-
     @Override
     public Cell toCell(char number) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (number == table[i][j]) {
-                    return new Cell(i, j);
-                }
-            }
-        }
-        return null;
+        final int value = number - '0' - 1;
+        return new Cell(value / 3, value % 3);
     }
 
     @Override
     public char toNumber(Cell cell) {
-        return table[cell.getRow()][cell.getCol()];
+        return (char) ('0' + (cell.getRow() * 3 + cell.getCol() + 1));
     }
 }
